efa97d8dc3bf0f9872cd8c9e79ce6d32f40a8c1e
Praharshhhhhhh <pktarsariya@gmail.com>
2026-05-14 15:47:21 +0530

    Update Transimiter_Code.ino

--- Transmitter/Transimiter_Code.ino
+++ Transmitter/Transimiter_Code.ino
@@ -1,8 +1,11 @@
+#include <Wire.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
-// NRF24L01 Pins
+// NRF24
 #define CE_PIN 7
 #define CSN_PIN 8
 
@@ -10,10 +13,8 @@ RF24 radio(CE_PIN, CSN_PIN);
 
 const byte address[6] = "00001";
 
-// ADXL335 Pins
-#define X_PIN A0
-#define Y_PIN A1
-#define Z_PIN A2
+// ADXL345
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 struct ControlData {
   char dir;
@@ -22,15 +23,24 @@ struct ControlData {
 
 ControlData data;
 
-// Smooth speed variables
 int currentSpeed = 0;
 int targetSpeed = 0;
 
 void setup() {
+
   Serial.begin(9600);
 
+  // Initialize ADXL345
+  if (!accel.begin()) {
+    Serial.println("ADXL345 not detected!");
+    while (1);
+  }
+
+  accel.setRange(ADXL345_RANGE_16_G);
+
+  // Initialize NRF24
   if (!radio.begin()) {
-    Serial.println("nRF24L01 not detected!");
+    Serial.println("NRF24L01 not detected!");
     while (1);
   }
 
@@ -38,51 +48,43 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   radio.stopListening();
 
-  Serial.println("Gesture Transmitter Ready");
+  Serial.println("Gesture Controller Ready");
 }
 
 void loop() {
 
-  int xValue = analogRead(X_PIN);
-  int yValue = analogRead(Y_PIN);
+  sensors_event_t event;
+  accel.getEvent(&event);
 
-  // Default stop
+  float x = event.acceleration.x;
+  float y = event.acceleration.y;
+
   data.dir = 'S';
   targetSpeed = 0;
 
-  // Center values around 512
-  int xOffset = xValue - 512;
-  int yOffset = yValue - 512;
+  // Calculate tilt strength
+  float tilt = max(abs(x), abs(y));
 
-  // Dead zone
-  if (abs(xOffset) < 40) xOffset = 0;
-  if (abs(yOffset) < 40) yOffset = 0;
-
-  // Calculate proportional speed
-  int tiltAmount = max(abs(xOffset), abs(yOffset));
-
-  targetSpeed = map(tiltAmount, 0, 350, 0, 255);
+  targetSpeed = map(tilt * 10, 0, 100, 0, 255);
   targetSpeed = constrain(targetSpeed, 0, 255);
 
-  // Gesture detection
-
   // Forward
-  if (yOffset > 80) {
+  if (y > 3) {
     data.dir = 'F';
   }
 
   // Backward
-  else if (yOffset < -80) {
+  else if (y < -3) {
     data.dir = 'B';
   }
 
   // Right
-  else if (xOffset > 80) {
+  else if (x > 3) {
     data.dir = 'R';
   }
 
   // Left
-  else if (xOffset < -80) {
+  else if (x < -3) {
     data.dir = 'L';
   }
 
@@ -92,29 +94,26 @@ void loop() {
     targetSpeed = 0;
   }
 
-  // Smooth acceleration / deceleration
-  if (currentSpeed < targetSpeed) {
+  // Smooth acceleration
+  if (currentSpeed < targetSpeed)
     currentSpeed += 3;
-  }
 
-  else if (currentSpeed > targetSpeed) {
+  if (currentSpeed > targetSpeed)
     currentSpeed -= 3;
-  }
 
   currentSpeed = constrain(currentSpeed, 0, 255);
 
-  // Send smoothed speed
   data.speed = currentSpeed;
 
-  // Transmit data
+  // Send data
   radio.write(&data, sizeof(data));
 
-  // Debugging
+  // Debug
   Serial.print("X: ");
-  Serial.print(xValue);
+  Serial.print(x);
 
   Serial.print(" Y: ");
-  Serial.print(yValue);
+  Serial.print(y);
 
   Serial.print(" Dir: ");
   Serial.print(data.dir);
